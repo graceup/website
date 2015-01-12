d205244e2b0b11878ca808ea7b9178d3e0508092
graceup <924443349@qq.com>
2015-01-12 20:20:55 +0800

    html 页面
    Signed-off-by: graceup <924443349@qq.com>

---
+++ test/js/website.js
@@ -0,0 +1,14 @@
+$(function() {
+	//Í¼Æ¬ÂÖ²¥
+	$('#flashbox').smallslider({
+		onImageStop : false,
+		switchEffect : 'fadeOut',
+		switchEase : 'easeOutBounce',
+		switchPath : 'up',
+		switchMode : 'hover',
+		textSwitch : 2,
+		textPosition : 'bottom',
+		textAlign : 'center'
+	});    
+    
+});
